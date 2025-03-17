f133e1385c63e81e8f0c99f2d2a073168924cf3c
lisa188 <lyu188@gmail.com>
2025-03-17 18:26:00 -0400

    rotate player based on pressed keys

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
 
