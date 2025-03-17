ad4ad3c70f6ad8c006eb23f1e323121c85e586e6
lisa188 <lyu188@gmail.com>
2025-03-17 00:15:02 -0400

    set framerate

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ import pygame
 from constants import *
 
 def main():
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     # initialize the pygame module
     pygame.init()
-
     # set new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    refresh_rate = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+            
+        screen.fill("black")
         pygame.display.flip()
+        
+        # limit to 60 FPS
+        dt = refresh_rate.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
